912ebad0f3a6f9a9bc4d3032a08f4368d9a73b83
RitaFer <rialf.ferreira@gmail.com>
2022-02-17 12:15:12 -0300

    navbar-footer

--- 02-costs/src/components/layout/Footer.js
+++ 02-costs/src/components/layout/Footer.js
@@ -1,8 +1,28 @@
+import {FaFacebook, FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+
 function Footer(){
   return(
-    <>
-    <p>Teste Footer</p>
-    </>
+    <footer className={styles.footer}>
+      <ul className={styles.social}>
+        <li>
+          <FaFacebook/>
+        </li>
+        <li>
+          <FaInstagram/>
+        </li>
+        <li>
+          <FaLinkedin/>
+        </li>
+        <li>
+          <FaGithub/>
+        </li>
+      </ul>
+      <p>
+        <span>Desenvolvido por Rita Ferreira</span>
+      </p>
+    </footer>
   )
 }
 
--- 02-costs/src/components/layout/NavBar.js
+++ 02-costs/src/components/layout/NavBar.js
@@ -1,17 +1,32 @@
-import { Link } from 'react-router-dom'
-// import styles from './NavBar.module.css'
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import styles from "./NavBar.module.css";
+import logo from "../../img/costs_logo.png";
 
-function NavBar(){
-  return(
-    <>
-    <ul>
-        <li><Link to='/'>Página Inicial</Link></li>
-        <li><Link to='/newproject'>Novo Projeto</Link></li>
-        <li><Link to='/company'>Empresa</Link></li>
-        <li><Link to='/contact'>Contato</Link></li>
-      </ul>
-    </>
-  )
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img className={styles.img} src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+      </Container>
+    </div>
+  );
 }
 
-export default NavBar
+export default Navbar;
