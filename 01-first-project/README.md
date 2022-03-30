76c782b17d1e9be92f70bd9f9df307f2464f68e3
Rita Ferreira <rialf.ferreira@gmail.com>
2022-03-29 22:38:55 -0300

    Delete 01-first-project directory

--- 01-first-project/README.md
+++
@@ -1,5 +0,0 @@
-# This is just a test project
-
-I used some concepts for learn about components in react. You can see other projects with a better build in other paths in this repository.
-
-### See you!
--- 01-first-project/src/components/Greeting.js
+++
@@ -1,9 +0,0 @@
-function Greeting({ name }) {
-  function generateGreeting (someName){
-    return `Olá ${someName}, Tudo bem por aí?`
-  }
-
-  return <>{name && <p>{generateGreeting(name)}</p>}</>
-}
-
-export default Greeting
